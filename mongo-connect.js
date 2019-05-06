e18c12d7612a5d9e829cc178d1b9d7ae83bb236d
Khan Usama <usama@iorta.in>
2019-05-06 18:06:05 +0530

    connection file is taking value from .env

--- mongo-connect.js
+++ mongo-connect.js
@@ -1,9 +1,10 @@
 // Connection with mongoDB
 
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const SERVER_IP = '127.0.0.1:27017'; // Localhost
-const DB_NAME = 'graph_db' // Keep is a database name which stores user notes
+const SERVER_IP = process.env.MONGO_DB_IP+':'+process.env.MONGO_PORT; // Localhost
+const DB_NAME = process.env.DB_NAME // Keep is a database name which stores user notes
 const DB_PATH = `mongodb://${SERVER_IP}/${DB_NAME}`
 
 mongoose.set('debug', true);
